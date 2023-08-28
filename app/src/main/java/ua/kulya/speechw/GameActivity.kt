64bc8fbe0ca0c872cc3a25550e83eb186e42d7b2
Oleg Vishnevskiy <vishnevskiyoleg@gmail.com>
2023-08-28 19:41:09 +0300

    game logic

--- app/src/main/java/ua/kulya/speechw/GameActivity.kt
+++ app/src/main/java/ua/kulya/speechw/GameActivity.kt
@@ -3,16 +3,22 @@ package ua.kulya.speechw
 import android.animation.Animator
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
 import android.graphics.Rect
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.TranslateAnimation
+import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.doOnLayout
@@ -23,6 +29,8 @@ import ua.kulya.speechw.databinding.ActivityGameBinding
 import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
+
+    private var score: Int = 1
 
     private lateinit var binding: ActivityGameBinding
     private var initialX = 0f
@@ -80,6 +88,13 @@ class GameActivity : AppCompatActivity() {
             }
         }, collisionCheckInterval.toLong())
 
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                checkScores()
+                handler.postDelayed(this, collisionCheckInterval.toLong())
+            }
+        }, collisionCheckInterval.toLong())
+
     }
 
     suspend fun setAnimation(){
@@ -99,6 +114,45 @@ class GameActivity : AppCompatActivity() {
         animateFalling(binding.elem12)
     }
 
+    private fun checkScores(){
+        if (score<=0){
+
+            val frameLayout = FrameLayout(this).apply {
+                layoutParams = FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.MATCH_PARENT,
+                    FrameLayout.LayoutParams.MATCH_PARENT
+                )
+                setBackgroundResource(R.drawable.background)
+            }
+
+            // Create the TextView and set its properties
+            val textView = TextView(this).apply {
+                text = "Game Over"
+                setTextColor(Color.WHITE)
+                gravity = Gravity.CENTER // This will center the text inside the TextView
+                textSize = 32f * resources.displayMetrics.scaledDensity
+            }
+
+            val imageView = ImageView(this).apply {
+                setImageResource(R.drawable.baseline_close_24)
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                // Set an OnClickListener (for demonstration purposes, I'm using a Toast, but you can replace it with your logic)
+                setOnClickListener {
+                    val intent = Intent(this@GameActivity, MenuActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+            frameLayout.addView(textView)
+            frameLayout.addView(imageView)
+            binding.root.addView(frameLayout)
+
+        }
+    }
+
     private fun checkCollision(imageView: ImageView) {
         val elemRect = Rect()
         imageView.getGlobalVisibleRect(elemRect) // Update the Rect position based on the view's bounds
@@ -110,11 +164,36 @@ class GameActivity : AppCompatActivity() {
             // Collision detected
             // Stop the falling animation
             
-            Log.d("123123", "Collision has been occurred")
             imageView.visibility = View.GONE
-            //imageView.clearAnimation()
+            changeScores(imageView)
+
         }
     }
+
+    private fun changeScores(imageView: ImageView) {
+        val tag = imageView.tag
+
+        Log.d("123123", "The tag is $tag")
+
+        when(tag){
+            R.drawable.stone1 -> { score -= 10 }
+            R.drawable.stone2 -> { score -= 10 }
+            R.drawable.stone3 -> { score -= 10 }
+            R.drawable.tree1 -> { score -= 5 }
+            R.drawable.tree2 -> { score -= 5 }
+            R.drawable.tree3 -> {  score -= 5 }
+            R.drawable.monster4 -> { score += 1 }
+            R.drawable.monster5 -> { score += 2 }
+            R.drawable.monster6 -> { score += 3 }
+            R.drawable.monster7 -> { score += 4 }
+            R.drawable.monster8 -> { score += 5 }
+            R.drawable.monster9 -> { score += 6 }
+            R.drawable.monster10 -> { score += 7 }
+        }
+
+        binding.scoresContainer.text = "Score is $score"
+    }
+
     private fun animateFalling(view: ImageView) {
         val screenHeight = resources.displayMetrics.heightPixels.toFloat()
 
@@ -128,8 +207,11 @@ class GameActivity : AppCompatActivity() {
                 }
 
                 override fun onAnimationEnd(animation: Animator) {
+                    val randomImage = getRandomImage()
+
                     view.translationY = 0f
-                    view.setImageResource(getRandomImage())
+                    view.setImageResource(randomImage)
+                    view.tag = randomImage
                     view.visibility = View.VISIBLE
                     animateFalling(view) // Restart the animation
                 }
